93ae3252c0397621b82df05b9905b715fc0b9e54
trigaux <trigaux@cisco.com>
2023-06-22 14:35:56 -0700

    Appeasing Ubuntu.

--- include/quicr/name.h
+++ include/quicr/name.h
@@ -2,15 +2,16 @@
 
 #include <cstdint>
 #include <cstdlib>
+#include <cstring>
 #include <stdexcept>
 #include <string>
 #include <string_view>
 #include <vector>
 
-namespace quicr {
+namespace quicr
+{
 
-constexpr uint64_t
-hexchar_to_uint(char x)
+constexpr uint64_t hexchar_to_uint(char x)
 {
     uint64_t y = 0;
     if ('0' <= x && x <= '9')
@@ -23,10 +24,8 @@ hexchar_to_uint(char x)
     return y;
 }
 
-template<typename T,
-         typename = typename std::enable_if<std::is_unsigned_v<T>, T>::type>
-constexpr char
-uint_to_hexchar(T b)
+template <typename T, typename = typename std::enable_if<std::is_unsigned_v<T>, T>::type>
+constexpr char uint_to_hexchar(T b)
 {
     char x = ' ';
     if (b > 9)
@@ -37,16 +36,15 @@ uint_to_hexchar(T b)
     return x;
 }
 
-template<typename T,
-         typename = typename std::enable_if<std::is_unsigned_v<T>, T>::type>
-constexpr T
-hex_to_uint(std::string_view x)
+template <typename T, typename = typename std::enable_if<std::is_unsigned_v<T>, T>::type>
+constexpr T hex_to_uint(std::string_view x)
 {
     if (x.starts_with("0x"))
         x.remove_prefix(2);
 
     T y = 0;
-    for (size_t i = 0; i < x.length(); ++i) {
+    for (size_t i = 0; i < x.length(); ++i)
+    {
         y *= 16ull;
         y += hexchar_to_uint(x[i]);
     }
@@ -54,13 +52,12 @@ hex_to_uint(std::string_view x)
     return y;
 }
 
-template<typename T,
-         typename = typename std::enable_if<std::is_unsigned_v<T>, T>::type>
-std::string
-uint_to_hex(T y)
+template <typename T, typename = typename std::enable_if<std::is_unsigned_v<T>, T>::type>
+std::string uint_to_hex(T y)
 {
     char x[sizeof(T) * 2 + 1] = "";
-    for (int i = sizeof(T) * 2 - 1; i >= 0; --i) {
+    for (int i = sizeof(T) * 2 - 1; i >= 0; --i)
+    {
         T b = y & 0x0F;
         x[i] = uint_to_hexchar(b);
         y -= b;
@@ -80,39 +77,36 @@ class Name
 
   public:
     Name() = default;
-    constexpr Name(const Name& other) = default;
-    Name(Name&& other) = default;
+    constexpr Name(const Name &other) = default;
+    Name(Name &&other) = default;
 
-    constexpr Name(uint8_t* data, size_t length)
+    constexpr Name(uint8_t *data, size_t length)
     {
         if (length > sizeof(Name) * 2)
-            throw std::invalid_argument(
-              "Byte array length cannot be longer than length of Name: " +
-              std::to_string(sizeof(Name) * 2));
+            throw std::invalid_argument("Byte array length cannot be longer than length of Name: " +
+                                        std::to_string(sizeof(Name) * 2));
 
         constexpr size_t size_of = sizeof(Name) / 2;
         std::memcpy(&_low, data, size_of);
         std::memcpy(&_hi, data + size_of, size_of);
     }
 
-    constexpr Name(const uint8_t* data, size_t length)
+    constexpr Name(const uint8_t *data, size_t length)
     {
         if (length > sizeof(Name) * 2)
-            throw std::invalid_argument(
-              "Byte array length cannot be longer than length of Name: " +
-              std::to_string(sizeof(Name) * 2));
+            throw std::invalid_argument("Byte array length cannot be longer than length of Name: " +
+                                        std::to_string(sizeof(Name) * 2));
 
         constexpr size_t size_of = sizeof(Name) / 2;
         std::memcpy(&_low, data, size_of);
         std::memcpy(&_hi, data + size_of, size_of);
     }
 
-    constexpr Name(const std::vector<uint8_t>& data)
+    constexpr Name(const std::vector<uint8_t> &data)
     {
         if (data.size() > sizeof(Name))
-            throw std::invalid_argument(
-              "Byte array length cannot be longer than length of Name: " +
-              std::to_string(sizeof(Name)));
+            throw std::invalid_argument("Byte array length cannot be longer than length of Name: " +
+                                        std::to_string(sizeof(Name)));
 
         constexpr size_t size_of = sizeof(Name) / 2;
         std::memcpy(&_low, data.data(), size_of);
@@ -125,24 +119,23 @@ class Name
             hex_value.remove_prefix(2);
 
         if (hex_value.length() > sizeof(Name) * 2)
-            throw std::invalid_argument("Hex string cannot be longer than " +
-                                        std::to_string(sizeof(Name) * 2) +
+            throw std::invalid_argument("Hex string cannot be longer than " + std::to_string(sizeof(Name) * 2) +
                                         " bytes");
 
-        if (hex_value.length() > sizeof(Name)) {
-            _hi = hex_to_uint<uint64_t>(
-              hex_value.substr(0, hex_value.length() - sizeof(Name)));
-            _low = hex_to_uint<uint64_t>(hex_value.substr(
-              hex_value.length() - sizeof(Name), sizeof(Name)));
-        } else {
+        if (hex_value.length() > sizeof(Name))
+        {
+            _hi = hex_to_uint<uint64_t>(hex_value.substr(0, hex_value.length() - sizeof(Name)));
+            _low = hex_to_uint<uint64_t>(hex_value.substr(hex_value.length() - sizeof(Name), sizeof(Name)));
+        }
+        else
+        {
             _hi = 0;
-            _low =
-              hex_to_uint<uint64_t>(hex_value.substr(0, hex_value.length()));
+            _low = hex_to_uint<uint64_t>(hex_value.substr(0, hex_value.length()));
         }
     }
 
-    constexpr Name& operator=(const Name& other) = default;
-    constexpr Name& operator=(Name&& other) = default;
+    constexpr Name &operator=(const Name &other) = default;
+    constexpr Name &operator=(Name &&other) = default;
 
     std::string to_hex() const
     {
@@ -156,8 +149,7 @@ class Name
     std::uint8_t operator[](std::size_t index) const
     {
         if (index >= sizeof(Name))
-            throw std::out_of_range(
-              "Cannot access index outside of max size of quicr::Name");
+            throw std::out_of_range("Cannot access index outside of max size of quicr::Name");
 
         if (index < sizeof(uint64_t))
             return (_low >> (index * 8)) & 0xff;
@@ -176,11 +168,14 @@ class Name
         if (value == 0)
             return *this;
 
-        if (value < sizeof_type_bits) {
+        if (value < sizeof_type_bits)
+        {
             _low = _low >> value;
             _low |= _hi << (sizeof_type_bits - value);
             _hi = _hi >> value;
-        } else {
+        }
+        else
+        {
             _low = _hi >> (value - sizeof_type_bits);
             _hi = 0;
         }
@@ -200,11 +195,14 @@ class Name
         if (value == 0)
             return *this;
 
-        if (value < sizeof_type_bits) {
+        if (value < sizeof_type_bits)
+        {
             _hi = _hi << value;
             _hi |= _low >> (sizeof_type_bits - value);
             _low = _low << value;
-        } else {
+        }
+        else
+        {
             _hi = _low << (value - sizeof_type_bits);
             _low = 0;
         }
@@ -221,7 +219,8 @@ class Name
 
     constexpr void operator+=(uint64_t value)
     {
-        if (_low + value < _low) {
+        if (_low + value < _low)
+        {
             ++_hi;
         }
         _low += value;
@@ -236,7 +235,8 @@ class Name
 
     constexpr void operator+=(Name value)
     {
-        if (_low + value._low < _low) {
+        if (_low + value._low < _low)
+        {
             ++_hi;
         }
         _low += value._low;
@@ -252,7 +252,8 @@ class Name
 
     constexpr void operator-=(uint64_t value)
     {
-        if (_low - value > _low) {
+        if (_low - value > _low)
+        {
             --_hi;
         }
         _low -= value;
@@ -267,10 +268,13 @@ class Name
 
     constexpr void operator-=(Name value)
     {
-        if (_hi - value._hi > _hi) {
+        if (_hi - value._hi > _hi)
+        {
             _hi = 0;
             --_low;
-        } else {
+        }
+        else
+        {
             _hi -= value._hi;
         }
         *this -= value._low;
@@ -309,7 +313,10 @@ class Name
         return name;
     }
 
-    constexpr void operator&=(uint64_t value) { _low &= value; }
+    constexpr void operator&=(uint64_t value)
+    {
+        _low &= value;
+    }
 
     constexpr Name operator|(uint64_t value) const
     {
@@ -318,58 +325,61 @@ class Name
         return name;
     }
 
-    constexpr void operator|=(uint64_t value) { _low |= value; }
+    constexpr void operator|=(uint64_t value)
+    {
+        _low |= value;
+    }
 
-    constexpr Name operator&(const Name& other) const
+    constexpr Name operator&(const Name &other) const
     {
         Name name = *this;
         name &= other;
         return name;
     }
 
-    constexpr void operator&=(const Name& other)
+    constexpr void operator&=(const Name &other)
     {
         _hi &= other._hi;
         _low &= other._low;
     }
 
-    constexpr Name operator|(const Name& other) const
+    constexpr Name operator|(const Name &other) const
     {
         Name name = *this;
         name |= other;
         return name;
     }
 
-    constexpr void operator|=(const Name& other)
+    constexpr void operator|=(const Name &other)
     {
         _hi |= other._hi;
         _low |= other._low;
     }
 
-    constexpr Name operator^(const Name& other) const
+    constexpr Name operator^(const Name &other) const
     {
         Name name = *this;
         name ^= other;
         return name;
     }
 
-    constexpr void operator^=(const Name& other)
+    constexpr void operator^=(const Name &other)
     {
         _hi ^= other._hi;
         _low ^= other._low;
     }
 
-    friend constexpr bool operator==(const Name& a, const Name& b)
+    friend constexpr bool operator==(const Name &a, const Name &b)
     {
         return a._hi == b._hi && a._low == b._low;
     }
 
-    friend constexpr bool operator!=(const Name& a, const Name& b)
+    friend constexpr bool operator!=(const Name &a, const Name &b)
     {
         return !(a == b);
     }
 
-    friend constexpr bool operator>(const Name& a, const Name& b)
+    friend constexpr bool operator>(const Name &a, const Name &b)
     {
         if (a._hi > b._hi)
             return true;
@@ -378,7 +388,7 @@ class Name
         return a._low > b._low;
     }
 
-    friend constexpr bool operator<(const Name& a, const Name& b)
+    friend constexpr bool operator<(const Name &a, const Name &b)
     {
         if (a._hi < b._hi)
             return true;
@@ -395,7 +405,7 @@ class Name
         return name;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Name& name)
+    friend std::ostream &operator<<(std::ostream &os, const Name &name)
     {
         os << name.to_hex();
         return os;
@@ -407,8 +417,7 @@ class Name
 };
 } // namespace quicr
 
-constexpr quicr::Name
-operator""_name(const char* x)
+constexpr quicr::Name operator""_name(const char *x)
 {
-    return { std::string_view(x) };
+    return {std::string_view(x)};
 }
